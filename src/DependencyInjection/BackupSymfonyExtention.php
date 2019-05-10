46df5f956195397ed9bee54c5d710a0faa73efc5
Florian PASQUEREAU <fpasquer@student.42.fr>
2019-05-10 13:59:47 +0000

    Update and rename SymfonyBackupExtention.php to BackupSymfonyExtention.php

--- src/DependencyInjection/BackupSymfonyExtention.php
+++ src/DependencyInjection/BackupSymfonyExtention.php
@@ -6,7 +6,7 @@ use ProxyManager\FileLocator\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class SymfonyBackupExtention extends Extension
+class BackupSymfonyExtention extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
